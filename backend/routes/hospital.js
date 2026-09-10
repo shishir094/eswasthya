96667767900c606fa82fa7f4cb85c0fc92581522
shishir094 <ritushishir04@gmail.com>
2026-09-11 03:28:57 +0545

    change in dns to default ipv4

--- backend/routes/hospital.js
+++ backend/routes/hospital.js
@@ -1,3 +1,5 @@
+import dns from 'node:dns';
+dns.setDefaultResultOrder('ipv4first');
 import express from 'express';
 import verifyHospital from '../middleware/hospitalAuth.js';
 import jwt from 'jsonwebtoken';
