eb495bf6102048b421f43f5bf08793418108062d
shishir094 <ritushishir04@gmail.com>
2026-09-13 00:37:43 +0545

    fix route errors

--- users/src/App.jsx
+++ users/src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/register" element={<Register />} />
     </Routes>
